72839686b3614132952b9530b7e2fe4ba301d806
JuniorEloy <franciscoeloyjr@gmail.com>
2020-04-30 10:51:50 -0300

    commit com maquinas

--- template/js/novopedido.js
+++ template/js/novopedido.js
@@ -10,6 +10,13 @@ var fragmento_checkbox = ' <div class="row">' +
                         '{{LABEL}}' +
                         '</div>';
 
+var fragmento_radio =   '<div class="form-check">'+
+                        '<input class="form-check-input" type="radio" name="exampleRadios" id="{{ID}}" value="option1">'+
+                        '<label class="form-check-label" for="exampleRadios1">'+
+                        '{{LABEL}}'+
+                        '</label>'+
+                        '</div>';
+                
 
 function carregaSoftware() {
     var user = localStorage.getItem("user");
@@ -20,9 +27,21 @@ function carregaSoftware() {
         fetch("http://localhost:8080/software/disponiveis")
             .then(res => res.json())
             .then(res => popula(res));
+        fetch("http://localhost:8080/maquinas")
+            .then(res => res.json())
+            .then(res => popula_maquina(res));
     }
 }
 
+function popula_maquina(lista){
+    strMaquina = '';
+    for (i=0; i<lista.length; i ++){
+        strMaquina_draft = fragmento_radio;
+        strMaquina += strMaquina_draft.replace("{{ID}}", lista[i].id)
+                                      .replace("{{LABEL}}", lista[i].maquina +" "+ lista[i].modelo + "; HD = " + lista[i].hd +"; Memoria = " + lista[i].ram+ "GB ; "+ lista[i].tamanhoTela+"\"");    
+    }
+    document.getElementById("listaMaquina").innerHTML = strMaquina;
+}
 
 function popula(lista) {
     strSoftwareList = '';
@@ -36,26 +55,27 @@ function popula(lista) {
 
 function gera_lista_softwares_escolhidos(){
     const checkboxes = document.querySelectorAll('input[name="software"]:checked');
+    const radio = document.querySelectorAll('input[name="exampleRadios"]:checked');
+    id_maquina = radio[0].id
+
     let softwares_list = [];
     checkboxes.forEach((checkbox) => {
         softwares_list.push(checkbox.value);
     });
 
-    Json_pedido = gera_JSON_pedido(softwares_list, $("#obs_pedido").val());
+    Json_pedido = gera_JSON_pedido(softwares_list, $("#obs_pedido").val(), id_maquina);
 
-    console.log(Json_pedido);
-
-
+    //console.log(Json_pedido);
 }
 
-function gera_JSON_pedido(lista_softwares, observacao){
+function gera_JSON_pedido(lista_softwares, observacao, id_maquina){
 var data = new Date();
 var dia         = data.getDate();           // 1-31
 var mes         = data.getMonth();          // 0-11 (zero=janeiro)
 var ano4        = data.getFullYear();       // 4 dígitos
 var str_data    = dia + '/' + (mes+1) + '/' + ano4;
 
-var usuario = JSON.parse(window.localStorage.getItem('user'));;
+var usuario = JSON.parse(window.localStorage.getItem('user'));
 
     dict_pedido = {
         "status": "N",
@@ -70,6 +90,12 @@ var usuario = JSON.parse(window.localStorage.getItem('user'));;
     }
 
     return dict_pedido;
+}
+
+//comando para remover o cook e redirecionar para login.html
+function logout() {
+    localStorage.removeItem("user");                
+    window.location = "./login.html"; 
 }
 
 //comando para adicionar os itens como icones do feather
